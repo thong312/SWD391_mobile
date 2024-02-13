055f91e35cd3c9de288613f2fd484af17217cf4e
thong312 <lyminhthong312@gmail.com>
2024-02-13 17:22:11 +0700

    firebase not done

--- mobile_learning/lib/main.dart
+++ mobile_learning/lib/main.dart
@@ -1,12 +1,15 @@
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
